b116380564c7081de86b165c167fe69441f163b1
Marvyn97 <marvynpaulin@gmail.com>
2022-04-06 15:15:58 +0200

    2st

--- js/main.js
+++ js/main.js
@@ -4,30 +4,20 @@ let ctx = myCanvas.getContext("2d");
 let c = new Disk(myCanvas, ctx);
 console.log(ctx);
 
-
-
 // fonction getMouseLocation(e) qui va retourner la position de la souris
 function getMouseLocation(e) {
-    mouse_X = e.clientX;
-    mouse_Y = e.clientY;
-    console.log("sdsd", mouse_X);
+  let mouse_X = e.clientX;
+  let mouse_Y = e.clientY;
 }
-
-
-
 
 // fonction onClickCanvas(e) qui va récuperer la position du clic et appeler differente méthodes pour génerer le dessin
-function onClickCanvas(){
-    console.log("qsdqsd");
-    this.draw();
+function onClickCanvas() {
+  console.log("qsdqsd");
+  this.draw();
 }
 
-
-
-// au chargement de la page, on va récupérer le canvas, le context et installer un écouteur 
+// au chargement de la page, on va récupérer le canvas, le context et installer un écouteur
 document.addEventListener("DomContentLoaded", () => {
-
-document.addEventListener("click", getMouseLocation(e));
-myCanvas.addEventListener("click", onClickCanvas());
-
+  document.addEventListener("click", getMouseLocation());
+  myCanvas.addEventListener("click", onClickCanvas());
 });
